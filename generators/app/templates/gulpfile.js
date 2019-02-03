043c4144965fa9c419735cba9454a843a01a264c
Vagrant Default User <vagrant@stretch.localdomain>
2019-02-03 16:36:53 +0000

    Include basic gulpfile.

---
+++ generators/app/templates/gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Node 
+var del = require('del');
+
+// Gulp plugins
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+// Paths
+var paths = {
+    index: {  
+	src: './src/index.html',
+	dest: './dist'
+    },
+};
+
+gulp.task('default', function () {
+    
+});
